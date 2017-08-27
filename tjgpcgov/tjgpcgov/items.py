944bcf012116badb04f50674214b075c3cfc08bc
Duhailong <1719812714@qq.com>
2017-08-27 21:04:25 +0800

    天津市政府采购中心&项目信息&招标公告

--- tjgpcgov/tjgpcgov/items.py
+++ tjgpcgov/tjgpcgov/items.py
@@ -18,11 +18,21 @@ class TjgpcgovItem(scrapy.Item):
 class tigpcItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    # 标题
     title  = scrapy.Field()
+    # 招标类型
     type  = scrapy.Field()
+    # 详情url
     url  = scrapy.Field()
+    # 发布时间
     time = scrapy.Field()
+    # 分类
     category  = scrapy.Field()
+    # 详细内容
     description  = scrapy.Field()
+    # 城市
+    city = scrapy.Field()
+    # tag
     tag  = scrapy.Field()
+
     
