41ee700efe934522f4687831b2d6d40e47e8c560
Duhailong <1719812714@qq.com>
2017-08-29 01:07:15 +0800

    update

--- tjgpcgov/tjgpcgov/spiders/cn_sxzfcg_www.py
+++ tjgpcgov/tjgpcgov/spiders/cn_sxzfcg_www.py
@@ -7,6 +7,8 @@ class CnSxzfcgWwwSpider(scrapy.Spider):
     name = "cn.sxzfcg.www"
     allowed_domains = ["http://www.sxzfcg.cn"]
     start_urls = ['http://www.sxzfcg.cn/view.php?nav=61']
+    # http://www.sxzfcg.cn/view.php?nav=61&page=1
+
 
     # 招标公告
     # http://www.sxzfcg.cn/view.php?nav=61
@@ -21,18 +23,19 @@ class CnSxzfcgWwwSpider(scrapy.Spider):
     def parse(self, response):
         print('-----------------------------------------------')
         # print(response.url)
+
         currentPage  = response.xpath("/html/body/table[1]/tr/td[2]/table/tr[1]/td[@class='c_pt']/table/tr[2]/td/div[@class='zt3']/div[@class='pager']/strong/font/text()").extract()[0]
         nextPage     = int(currentPage)+1
         print(currentPage)
         print(nextPage)
 
 
-        totalPage    = response.xpath("/html/body/table[1]/tr/td[2]/table/tr[1]/td[@class='c_pt']/table/tr[2]/td/div[@class='zt3']/div[@class='pager']/a[text()='最后一页'/@href").extract()[0]
-        # totalPage    = totalPage.split('=', 2)[2] 
-        # page_next = response.xpath('//*[@id="content"]//ul[@class="pagination"]/li/a[text()="下一页"]').extract()
+        totalPage    = response.xpath("/html/body/table[1]/tr/td[2]/table/tr[1]/td[@class='c_pt']/table/tr[2]/td/div[@class='zt3']/div[@class='pager']/a[text()='最后一页 »']/@href").extract()[0]
+        totalPage    = totalPage.split('=', 2)[2]
         print('total---'+totalPage)
 
-        # next_url = response.url.split('=',2)[0]+'='+response.url.split('=',2)[1]+'&page='+str(nextPage)
+
+        next_url = response.url.split('=',2)[0]+'='+response.url.split('=',2)[1]+'&page='+str(nextPage)
         next_url = 'http://www.sxzfcg.cn/view.php?nav=61&page='+str(nextPage)
         print(next_url)
         print('-----------------------------------------------')
