49bd8435b6e8f4577edf12e381dbf329f2842f75
Duhailong <1719812714@qq.com>
2017-08-29 23:19:26 +0800

    update

--- tjgpcgov/tjgpcgov/spiders/cn_org_act_www.py
+++ tjgpcgov/tjgpcgov/spiders/cn_org_act_www.py
@@ -7,5 +7,8 @@ class CnOrgActWwwSpider(scrapy.Spider):
     allowed_domains = ["www.act.org.cn"]
     start_urls = ['http://www.act.org.cn/']
     # 安徽省建筑工程招标投标监管网
+
+    # 招标公告
+    # 中标公示
     def parse(self, response):
         pass
