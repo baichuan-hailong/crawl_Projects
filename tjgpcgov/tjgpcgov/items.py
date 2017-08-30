c785191585af79a438c50d33a35d4131af7f24e7
Du Hailong <1719812714@qq.com>
2017-08-30 10:16:22 +0800

    update

--- tjgpcgov/tjgpcgov/items.py
+++ tjgpcgov/tjgpcgov/items.py
@@ -15,24 +15,59 @@ class TjgpcgovItem(scrapy.Item):
 
 
 
-class tigpcItem(scrapy.Item):
+class BiddenItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+
     # 标题
     title  = scrapy.Field()
     # 招标类型
     type  = scrapy.Field()
-    # 详情url
-    url  = scrapy.Field()
-    # 发布时间
-    time = scrapy.Field()
     # 分类
     category  = scrapy.Field()
     # 详细内容
     description  = scrapy.Field()
+    # 详情url
+    url  = scrapy.Field()
+    # 发布时间
+    issue_at = scrapy.Field()
+    # 城市
+    city = scrapy.Field()
+    # tag
+    tag  = scrapy.Field()
+
+    bidder = scrapy.Field()
+    
+    attachment  = scrapy.Field()
+
+
+class tigpcItem(scrapy.Item):
+    # define the fields for your item here like:
+
+    # 标题
+    title  = scrapy.Field()
+    # 招标类型
+    type  = scrapy.Field()
+    # 分类
+    category  = scrapy.Field()
+    # 详细内容
+    description  = scrapy.Field()
+    # 详情url
+    url  = scrapy.Field()
+    # 发布时间
+    time = scrapy.Field()
     # 城市
     city = scrapy.Field()
     # tag
     tag  = scrapy.Field()
 
     
+    # title  = scrapy.Field()
+    # type  = scrapy.Field()
+    # category  = scrapy.Field()
+    # description  = scrapy.Field()
+    # tag  = scrapy.Field()
+    # url  = scrapy.Field()
+    # attachment  = scrapy.Field()
+    # city  = scrapy.Field()
+    # bidder = scrapy.Field()
+    # issue_at = scrapy.Field()
