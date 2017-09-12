9cea9ed759b3bade7e17d2c60098ee1731afbcde
Du Hailong <1719812714@qq.com>
2017-09-12 19:28:51 +0800

    update

--- tjgpcgov/tjgpcgov/spiders/cn_com_jszb_www.py
+++ tjgpcgov/tjgpcgov/spiders/cn_com_jszb_www.py
@@ -8,41 +8,47 @@ import re
 
 
 class CnComJszbWwwSpider(scrapy.Spider):
-	# 江苏省建设工程招标网
+    # 江苏省建设工程招标网
     name = "cn.com.jszb.www"
     allowed_domains = ["www.jszb.com.cn"]
-    # start_urls = ['http://www.jszb.com.cn/jszb/YW_info/ZhaoBiaoGG/MoreInfo_ZBGG.aspx?categoryNum=012',]
-
     start_urls = ['http://www.jszb.com.cn/jszb']
-
 
     req_type = ["ZhaoBiaoGG/MoreInfo_ZBGG.aspx",
                 "ZuiGaoXJ/MoreInfo_ZGXJ.aspx",
                 "ZiGeYS/MoreInfo_ZGYS.aspx",
                 "HouXuanRenGS/MoreInfo_HxrGS.aspx",
                 "ZhongBiaoGS/MoreInfo_ZBGS.aspx",
-                "OtherInfo/MoreInfo_Zjfb.aspx",
-                "DBJieGuoGS/MoreInfo_DBJieGuoGS.aspx"]
+                "OtherInfo/MoreInfo_Zjfb.aspx"]
 
-    category = ["招标公告","最高限价公示","未入围公示","中标候选人公示","中标人公告","其他信息公告","评标、定标结果公示"]
+    category = ["招标公告","最高限价公示","未入围公示","中标候选人公示","中标人公告","其他信息公告"]
     industry = ["房屋建筑施工","市政工程施工","单独装饰装修施工","园林绿化","工程监理","设计","材料设备采购","电力","水利","机电设备","其它","招标终止"]
     
     def start_requests(self):
-        print('@___________________________________ start_requests fire ____________')
+        print('@___________________________________ Start ____________')
         for index,type_url in enumerate(self.req_type):
-            # print('http://www.jszb.com.cn/jszb/YW_info/'+type_url+'?categoryNum=012')
-            # print(index)
-            # print(self.category[index])
             type = self.category[index]
             yield Request('http://www.jszb.com.cn/jszb/YW_info/'+type_url+'?categoryNum=012', callback=self.request_category,meta={"type":type})
+
+            # if type=='招标公告':
+            #     for page in range(10):
+            #         postdata = {}
+            #         postdata['__EVENTTARGET']   = 'MoreInfoList1$Pager'
+            #         postdata['__EVENTARGUMENT'] = str(page)
+            #         yield FormRequest(
+            #             'http://www.jszb.com.cn/jszb/YW_info/'+type_url+'?categoryNum=012',
+            #             formdata = postdata,
+            #             callback = self.request_category,
+            #             meta={"type":type},
+            #             dont_filter=True
+            #             )
+            # else:
+            #     yield Request('http://www.jszb.com.cn/jszb/YW_info/'+type_url+'?categoryNum=012', callback=self.request_category,meta={"type":type})
 
 
     def request_category(self, response):
         # print('***************'+response.meta['type']+'***************')
-        if response.meta['type']=='招标公告' or response.meta['type']=='未入围公示':
+        if response.meta['type']=='招标公告':
             for tr in response.xpath("/html/body/form[@id='form1']/table[@class='tab'][3]/tr/td[4]/table/tr[@id='show01']/td[@class='border2']/table[@id='MoreInfoList1_moreinfo']/tr[4]/td[@id='MoreInfoList1_tdcontent']/table[@id='MoreInfoList1_DataGrid1']/tr[@class='moreinfoline']").extract():
-                # print(tr)
-                # print('-------------')
                 # Title
                 title = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
                 title = title.split(']')[1]
@@ -50,8 +56,6 @@ class CnComJszbWwwSpider(scrapy.Spider):
                 city = city.split(']')[0]
                 city = city.split('[')[1]
                 city = '江苏'+city
-                # print(city)
-                
                 # Detail url
                 onlickstr = Selector(text=tr).xpath('//td[2]/a/@onclick').extract()[0]
                 onlickstr = onlickstr.split('..')[1]
@@ -63,10 +67,10 @@ class CnComJszbWwwSpider(scrapy.Spider):
                 category = Selector(text=tr).xpath('//td[3]/text()').extract()[0]
                 # issue_at(发布时间)
                 issue_at = Selector(text=tr).xpath('//td[4]/text()').extract()[0]
-        
                 yield Request(detailurl, callback=self.parse_item,meta={"title":title,"type":type,"category":category,"issue_at":issue_at,"city":city,"detailurl":detailurl})
 
-        elif response.meta['type']=='最高限价公示':
+        elif response.meta['type']=='最高限价公示' or response.meta['type']=='未入围公示':
+            # "/html/body/table[@class='tab'][3]/tr/td[4]/table/tr[2]/td[@class='border2']/form[@id='ctl00']/table[@id='MoreInfoList1_moreinfo']/tr[4]/td[@id='MoreInfoList1_tdcontent']/table[@id='MoreInfoList1_DataGrid1']/tr[@class='moreinfoline']"
             for tr in response.xpath("/html/body/table[@class='tab'][3]/tr/td[4]/table/tr[2]/td[@class='border2']/form[@id='ctl00']/table[@id='MoreInfoList1_moreinfo']/tr[4]/td[@id='MoreInfoList1_tdcontent']/table[@id='MoreInfoList1_DataGrid1']/tr[@class='moreinfoline']").extract():
                 # Title
                 title = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
@@ -75,8 +79,6 @@ class CnComJszbWwwSpider(scrapy.Spider):
                 city = city.split(']')[0]
                 city = city.split('[')[1]
                 city = '江苏'+city
-                # print(city)
-                
                 # Detail url
                 onlickstr = Selector(text=tr).xpath('//td[2]/a/@onclick').extract()[0]
                 onlickstr = onlickstr.split('..')[1]
@@ -88,11 +90,52 @@ class CnComJszbWwwSpider(scrapy.Spider):
                 category = Selector(text=tr).xpath('//td[3]/text()').extract()[0]
                 # issue_at(发布时间)
                 issue_at = Selector(text=tr).xpath('//td[4]/text()').extract()[0]
-        
                 yield Request(detailurl, callback=self.parse_item,meta={"title":title,"type":type,"category":category,"issue_at":issue_at,"city":city,"detailurl":detailurl})
 
-        elif response.meta['type']=='中标候选人公示':
-            for tr in response.xpath("/html/body/table[@class='tab'][3]/tr/td[4]/table/tr[2]/td[@class='border2']/form[@id='ctl00']/table[@id='MoreInfoList1_moreinfo']/tr[4]/td[@id='MoreInfoList1_tdcontent']/table[@id='MoreInfoList1_DataGrid1']/tr[@class='moreinfoline']").extract():
+        elif response.meta['type']=='中标候选人公示':       
+            for tr in response.xpath("/html/body/table[@class='tab'][3]/tr/td[4]/table/tr[2]/td[@class='border2']/form[@id='ctl00']/table[@id='MoreInfoList_HXRGS1_moreinfo']/tr[4]/td[@id='MoreInfoList_HXRGS1_tdcontent']/table[@id='MoreInfoList_HXRGS1_DataGrid1']/tr[@class='moreinfoline']").extract():
+                # Title
+                title = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
+                title = title.split(']')[1]
+                city = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
+                city = city.split(']')[0]
+                city = city.split('[')[1]
+                city = '江苏'+city
+                # Detail url
+                onlickstr = Selector(text=tr).xpath('//td[2]/a/@onclick').extract()[0]
+                onlickstr = onlickstr.split('..')[1]
+                onlickstr = onlickstr.split('"')[0]
+                detailurl = 'http://www.jszb.com.cn/jszb/YW_info'+onlickstr
+                # Type
+                type = response.meta['type']
+                # Category
+                category = Selector(text=tr).xpath('//td[3]/text()').extract()[0]
+                # issue_at(发布时间)
+                issue_at = Selector(text=tr).xpath('//td[4]/text()').extract()[0]
+                yield Request(detailurl, callback=self.parse_item,meta={"title":title,"type":type,"category":category,"issue_at":issue_at,"city":city,"detailurl":detailurl})
+        elif response.meta['type']=='中标人公告':                             
+            for tr in response.xpath("/html/body/table[@class='tab'][2]/tr/td[4]/table/tr[2]/td[@class='border2']/form[@id='ctl00']/table[@id='MoreInfoList1_moreinfo']/tr[4]/td[@id='MoreInfoList1_tdcontent']/table[@id='MoreInfoList1_DataGrid1']/tr[@class='moreinfoline']").extract():
+                # Title
+                title = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
+                title = title.split(']')[1]
+                city = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
+                city = city.split(']')[0]
+                city = city.split('[')[1]
+                city = '江苏'+city
+                # Detail url
+                onlickstr = Selector(text=tr).xpath('//td[2]/a/@onclick').extract()[0]
+                onlickstr = onlickstr.split('..')[1]
+                onlickstr = onlickstr.split('"')[0]
+                detailurl = 'http://www.jszb.com.cn/jszb/YW_info'+onlickstr
+                # Type
+                type = response.meta['type']
+                # Category
+                category = Selector(text=tr).xpath('//td[3]/text()').extract()[0]
+                # issue_at(发布时间)
+                issue_at = Selector(text=tr).xpath('//td[4]/text()').extract()[0]
+                yield Request(detailurl, callback=self.parse_item,meta={"title":title,"type":type,"category":category,"issue_at":issue_at,"city":city,"detailurl":detailurl})
+        elif response.meta['type']=='其他信息公告':      
+            for tr in response.xpath("/html/body/table[@class='tab'][3]/tr/td[4]/table/tr[2]/td[@class='border2']/form[@id='ctl00']/table[@id='MoreInfoList_Zjfb1_moreinfo']/tr[4]/td[@id='MoreInfoList_Zjfb1_tdcontent']/table[@id='MoreInfoList_Zjfb1_DataGrid1']/tr[@class='moreinfoline']").extract():
                 # Title
                 title = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
                 title = title.split(']')[1]
@@ -116,103 +159,49 @@ class CnComJszbWwwSpider(scrapy.Spider):
         
                 yield Request(detailurl, callback=self.parse_item,meta={"title":title,"type":type,"category":category,"issue_at":issue_at,"city":city,"detailurl":detailurl})
 
-
-
-        elif response.meta['type']=='中标人公告':
-            for tr in response.xpath("/html/body/table[@class='tab'][3]/tr/td[4]/table/tr[2]/td[@class='border2']/form[@id='ctl00']/table[@id='MoreInfoList1_moreinfo']/tr[4]/td[@id='MoreInfoList1_tdcontent']/table[@id='MoreInfoList1_DataGrid1']/tr[@class='moreinfoline']").extract():
-                # Title
-                title = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
-                title = title.split(']')[1]
-                city = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
-                city = city.split(']')[0]
-                city = city.split('[')[1]
-                city = '江苏'+city
-                # print(city)
-                
-                # Detail url
-                onlickstr = Selector(text=tr).xpath('//td[2]/a/@onclick').extract()[0]
-                onlickstr = onlickstr.split('..')[1]
-                onlickstr = onlickstr.split('"')[0]
-                detailurl = 'http://www.jszb.com.cn/jszb/YW_info'+onlickstr
-                # Type
-                type = response.meta['type']
-                # Category
-                category = Selector(text=tr).xpath('//td[3]/text()').extract()[0]
-                # issue_at(发布时间)
-                issue_at = Selector(text=tr).xpath('//td[4]/text()').extract()[0]
-        
-                yield Request(detailurl, callback=self.parse_item,meta={"title":title,"type":type,"category":category,"issue_at":issue_at,"city":city,"detailurl":detailurl})
-
-
-
-        elif response.meta['type']=='其他信息公告':
-            for tr in response.xpath("/html/body/table[@class='tab'][3]/tr/td[4]/table/tr[2]/td[@class='border2']/form[@id='ctl00']/table[@id='MoreInfoList1_moreinfo']/tr[4]/td[@id='MoreInfoList1_tdcontent']/table[@id='MoreInfoList1_DataGrid1']/tr[@class='moreinfoline']").extract():
-                # Title
-                title = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
-                title = title.split(']')[1]
-                city = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
-                city = city.split(']')[0]
-                city = city.split('[')[1]
-                city = '江苏'+city
-                # print(city)
-                
-                # Detail url
-                onlickstr = Selector(text=tr).xpath('//td[2]/a/@onclick').extract()[0]
-                onlickstr = onlickstr.split('..')[1]
-                onlickstr = onlickstr.split('"')[0]
-                detailurl = 'http://www.jszb.com.cn/jszb/YW_info'+onlickstr
-                # Type
-                type = response.meta['type']
-                # Category
-                category = Selector(text=tr).xpath('//td[3]/text()').extract()[0]
-                # issue_at(发布时间)
-                issue_at = Selector(text=tr).xpath('//td[4]/text()').extract()[0]
-        
-                yield Request(detailurl, callback=self.parse_item,meta={"title":title,"type":type,"category":category,"issue_at":issue_at,"city":city,"detailurl":detailurl})
-
-
-
-        elif response.meta['type']=='评标、定标结果公示':
-            for tr in response.xpath("/html/body/table[@class='tab'][3]/tr/td[4]/table/tr[2]/td[@class='border2']/form[@id='ctl00']/table[@id='MoreInfoList1_moreinfo']/tr[4]/td[@id='MoreInfoList1_tdcontent']/table[@id='MoreInfoList1_DataGrid1']/tr[@class='moreinfoline']").extract():
-                # Title
-                title = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
-                # City
-                title = title.split(']')[1]
-                city = Selector(text=tr).xpath('//td[2]/a/text()').extract()[0]
-                city = city.split(']')[0]
-                city = city.split('[')[1]
-                city = '江苏'+city
-                # print(city)
-                
-                # Detail url
-                onlickstr = Selector(text=tr).xpath('//td[2]/a/@onclick').extract()[0]
-                onlickstr = onlickstr.split('..')[1]
-                onlickstr = onlickstr.split('"')[0]
-                detailurl = 'http://www.jszb.com.cn/jszb/YW_info'+onlickstr
-                # Type
-                type = response.meta['type']
-                # Category
-                category = Selector(text=tr).xpath('//td[3]/text()').extract()[0]
-                # issue_at(发布时间)
-                issue_at = Selector(text=tr).xpath('//td[4]/text()').extract()[0]
-        
-                yield Request(detailurl, callback=self.parse_item,meta={"title":title,"type":type,"category":category,"issue_at":issue_at,"city":city,"detailurl":detailurl})
 
 
     def parse_item(self, response):
         # print("********************Detail*************************")
+        print("********************Detail*************************"+response.meta['type']+"********************Detail*************************")
+
+        des = ''
+        if response.meta['type']=='招标公告':
+            for str in response.xpath("/html/body/form[@id='Form1']/font/table/tr/td[@id='tdContainer']/table[@id='Table1']/tr/td//text()").extract():
+                str = str.strip().replace("\n", "")
+                des = des+str+'\n'
+        elif response.meta['type']=='最高限价公示' or response.meta['type']=='未入围公示':
+            for str in response.xpath("/html/body/form[@id='Form1']/font/table/tr/td/table[@id='Table1']/tr/td//text()").extract():
+                str = str.strip().replace("\n", "")
+                des = des+str+'\n' 
+        elif response.meta['type']=='中标人公告':  
+            # "/html/body/form[@id='Form1']/font/table/tr/td/table[@id='Table1']/tr/td//text()"
+            for str in response.xpath("/html/body/form[@id='Form1']/font/table/tr/td/table[@id='Table1']/tr/td//text()").extract():
+                str = str.strip().replace("\n", "")
+                des = des+str+'\n'                           
+        elif response.meta['type']=='其他信息公告':
+            for str in response.xpath("/html/body/form[@id='Form1']/font/table/tr/td/table[@id='Table1']/tr/td//text()").extract():
+                str = str.strip().replace("\n", "")
+                des = des+str+'\n' 
+
+        # print(des)
+        
+        # expire_at = re.search("公告结束时间+",des)
+        # print(expire_at.group())
+        # print(re.search(r'(公告开始时间\d+年\d+月\d+日)',des).group(0))
+        # print(re.search(r'公告开始时间(\w+)',des,re.U))
+        # 公告开始时间2017年9月12日公告结束时间2017年9月15日
+
         data = {}
         data['title'] = response.meta['title']
         data['type'] = response.meta['type']
         data['category'] = re.sub(r'[\n\t]+','\n',response.meta['category']).strip()
         data['issue_at'] = re.sub(r'[\n\t]+','\n',response.meta['issue_at']).strip()
-
         data['city'] = response.meta['city']
         data['detailurl'] = response.meta['detailurl']
-
-        
-        # print(data)
-        return data
+        data['description'] = des
+        print(data)
+        # return data
 
 
 
--- tjgpcgov/tjgpcgov/spiders/com_njhwzbb_www.py
+++ tjgpcgov/tjgpcgov/spiders/com_njhwzbb_www.py
@@ -2,39 +2,47 @@
 # Author --- 百川 ---
 import scrapy
 from scrapy.selector import Selector
+from scrapy.http import Request, FormRequest
 
 
 class ComNjhwzbbWwwSpider(scrapy.Spider):
-    name = "com.njhwzbb.www"
-    allowed_domains = ["www.njhwzbb.com"]
-    # start_urls = ['http://www.njhwzbb.com/app/index/column/indexColumnBidAnnounce.html?currentCode=4100000&parentCode=102300&childCode=102320&time=1503998651','http://www.njhwzbb.com/app/index/column/indexColumnBidAnnounce.html?currentCode=4100000&parentCode=102300&childCode=102431&time=1503998685']
-    start_urls = ['http://www.njhwzbb.com/app/index/column/indexColumnBidAnnounce.html?currentCode=4100000&parentCode=102300&childCode=102320']
     # 南京货物招标投标监督平台
+    name            = "com.njhwzbb.www"
+    allowed_domains = ["www.njhwzbb.com"]
+    start_urls      = ['http://www.njhwzbb.com']
 
-    # 招标公告
-    # http://www.njhwzbb.com/app/index/column/indexColumnBidAnnounce.html?currentCode=4100000&parentCode=102300&childCode=102320&time=1503998651
 
-    # 资审公告
-    # http://www.njhwzbb.com/app/index/column/indexColumnBidAnnounce.html?currentCode=4100000&parentCode=102300&childCode=102340&time=1503998669
+    childCode= ["102320","102340","102431"]
+    category = ["招标公告","资审公告","中标公告"]
 
-    # 中标公告
-    # http://www.njhwzbb.com/app/index/column/indexColumnBidAnnounce.html?currentCode=4100000&parentCode=102300&childCode=102431&time=1503998685
 
-    def parse(self, response):
-        # pass
-        print('@___________________________________@')
+    def start_requests(self):
+        print('@___________________________________Start___________________________________@')
+        for index,child_code in enumerate(self.childCode):
+            type = self.category[index]
+            yield Request('http://www.njhwzbb.com/app/index/column/indexColumnBidAnnounce.html?currentCode=4100000&parentCode=102300&childCode='+child_code, callback=self.request_category,meta={"type":type})
+
+
+    def request_category(self, response):
+        print(response.meta['type'])
         print(response.url)
-        type = '中标公告'
-        if response.url == 'http://www.njhwzbb.com/app/index/column/indexColumnBidAnnounce.html?currentCode=4100000&parentCode=102300&childCode=102320&time=1503998651':
-            # pass
-            type = '招标公告'
-        print(type)
 
-        # /html/body/div/div[@id='BidSupervisionPage']/div[@class='bidSupervisionSection']/div[@class='bidSupervisionMain']/div[@id='conter']/div[@id='help-right']/div[@class='right-content-list-box']/div[@class='right-content-list'][1]/div[@class='right-content-left']
-        print(response.xpath("/html/body/div/div[@id='BidSupervisionPage']/div[@class='bidSupervisionSection']/div[@class='bidSupervisionMain']/div[@id='conter']/div[@id='help-right']/div[@class='right-content-list-box']"))
-        # right-content-right
-        # title = response.xpath("/html/body/div/div[@id='BidSupervisionPage']/div[@class='bidSupervisionSection']/div[@class='bidSupervisionMain']/div[@id='conter']/div[@id='help-right']/div[@class='right-content-list-box']/div[@class='right-content-list']/div[@class='right-content-left']")
-        # print(title)
-        # title = Selector(response=response).xpath("//div[@class='right-content-left']").extract()
-        # print(title)
-        print('@___________________________________@')
+        if response.meta['type']=='招标公告':
+            print('111')
+            print(response.xpath("/html/body/div/div[@id='BidSupervisionPage']/div[@class='bidSupervisionSection']/div[@class='bidSupervisionMain']/div[@class='mainHead']/div[@class='mainHeadTabs']/ul[@class='lt tabsUl']/li[@class='lt tabLi'][1]").extract())
+            print(response.body)
+        elif response.meta['type']=='中标公告':
+            print('222')
+            print(response.xpath("/html/body/div/div[@id='BidSupervisionPage']/div[@class='bidSupervisionSection']/div[@class='bidSupervisionMain']/div[@id='conter']/div[@id='help-right']/div[@class='right-content-list-box']/div[@class='right-content-list']").extract())
+
+        elif response.meta['type']=='资审公告':
+            print('3333')
+            print(response.xpath("/html/body/div/div[@id='BidSupervisionPage']/div[@class='bidSupervisionSection']/div[@class='bidSupervisionMain']/div[@id='conter']/div[@id='help-right']/div[@class='right-content-list-box']/div[@class='right-content-list']").extract())
+
+
+
+
+
+
+
+
